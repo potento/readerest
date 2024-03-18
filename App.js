d3d496a08a73c532734c6e7228d664e817007e49
potento <pizzaconqueso@protonmail.com>
2024-03-17 19:30:42 -0600

    base project

--- App.js
+++ App.js
@@ -1,20 +1,81 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { StatusBar, Text, View, TextInput, TouchableOpacity } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
+import { styles } from './styles';
 
 export default function App() {
+  const [inputText, setInputText] = useState('');
+  const [words, setWords] = useState([]);
+  const [wordIndex, setWordIndex] = useState(0);
+  const [speed, setSpeed] = useState(0.5);
+  const [isReading, setIsReading] = useState(false);
+
+  useEffect(() => {
+    let timer;
+
+    if (isReading) {
+      timer = setInterval(() => {
+        setWordIndex((prevIndex) => prevIndex + 1);
+      }, speed * 1000);
+    }
+
+    return () => clearInterval(timer);
+  }, [isReading, wordIndex, speed]);
+
+  const handleRead = () => {
+    const inputWords = inputText.trim().split(/\s+/);
+  
+    if (inputWords.length > 0) {
+      setWords(inputWords);
+      setIsReading(true);
+    }
+  };  
+
+  const handlePause = () => {
+    setIsReading(false);
+  };
+
+  const increaseSpeed = () => {
+    setSpeed((prevSpeed) => prevSpeed + 0.05);
+  };
+
+  const decreaseSpeed = () => {
+    if (speed > 0.05) {
+      setSpeed((prevSpeed) => prevSpeed - 0.05);
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <TextInput
+        placeholder="Pega un texto largo aquí"
+        multiline
+        style={styles.input}
+        onChangeText={(text) => setInputText(text)}
+        value={inputText}
+        placeholderTextColor="#ccc"
+      />
+      <View style={styles.buttonsContainer}>
+        <TouchableOpacity style={styles.button} onPress={handleRead}>
+          <Ionicons name="play" size={24} color="#1a1f1e" />
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.button} onPress={handlePause}>
+          <Ionicons name="pause" size={24} color="#1a1f1e" />
+        </TouchableOpacity>
+        <TouchableOpacity style={[styles.button, styles.speedButton]} onPress={decreaseSpeed}>
+          <Ionicons name="arrow-down" size={24} color="#1a1f1e" />
+        </TouchableOpacity>
+        <TouchableOpacity style={[styles.button, styles.speedButton]} onPress={increaseSpeed}>
+          <Ionicons name="arrow-up" size={24} color="#1a1f1e" />
+        </TouchableOpacity>
+      </View>
+      <View style={styles.textContainer}>
+        {words.length > 0 && (
+          <Text style={styles.word}>{words[wordIndex]}</Text>
+        )}
+      </View>
+      <Text style={styles.speedText}>Velocidad: {speed.toFixed(2)} segundos por palabra </Text>
       <StatusBar style="auto" />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ styles.js
@@ -0,0 +1,52 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+    backgroundColor: '#1a1f1e',
+  },
+  input: {
+    height: 100,
+    borderWidth: 1,
+    borderColor: '#ccc',
+    marginBottom: 20,
+    paddingHorizontal: 10,
+    fontSize: 16,
+    color: '#fff',
+  },
+  buttonsContainer: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    marginBottom: 20,
+  },
+  button: {
+    backgroundColor: '#6cbdb5',
+    borderRadius: 30,
+    padding: 15,
+    marginHorizontal: 10,
+  },
+  speedButton: {
+    backgroundColor: '#93ccc6',
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+  textContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  word: {
+    fontSize: 50,
+    fontWeight: 'bold',
+    color: '#e3dfba',
+  },
+  speedText: {
+    fontSize: 16,
+    color: '#fff',
+    textAlign: 'center',
+  },
+});
